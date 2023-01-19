dd5b4fbd2c43122d39f866dd0eb477b268186d7a
TimeIsUnending <64928915+TimeIsUnending@users.noreply.github.com>
2023-01-19 13:38:10 -0600

    Update AutoMatchmakingMod_Installer.bat
    Making new update to make the installer work better as well as for installing the new version of AMM I'm releasing today lol (I'm going to make a new/better installer soon just doing this for now since it's simple).

--- AutoMatchmakingMod_Installer.bat
+++ AutoMatchmakingMod_Installer.bat
@@ -1,19 +1,21 @@
 @echo off
 IF NOT EXIST Titanfall2.exe (
-    echo "Wrong folder, only execute this script in your Titanfall2 game folder"
+    echo "Installer executed in the wrong folder, make sure you are running this in your Titanfall2 install folder!"
     pause
     exit
 )
 curl -LO https://github.com/R2Northstar/Northstar/releases/download/v1.11.1/Northstar.release.v1.11.1.zip
 tar -xf Northstar.release.v1.11.1.zip
 del discord_game_sdk.dll
-rmdir /s /q "R2Northstar/plugins"
-rmdir /s /q "R2Northstar/mods"
-mkdir "R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.3"
-cd R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.3
-curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.3/TimeIsUnending.AutoMatchmaking.1.0.3.zip
-tar -xf TimeIsUnending.AutoMatchmaking.1.0.3.zip
-del TimeIsUnending.AutoMatchmaking.1.0.3.zip
+rmdir /s /q "R2Northstar/plugins/DiscordRPC.dll"
+rmdir /s /q "R2Northstar/mods/Northstar.Client"
+rmdir /s /q "R2Northstar/mods/Northstar.Custom"
+rmdir /s /q "R2Northstar/mods/Northstar.CustomServers"
+mkdir "R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.4"
+cd R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.4
+curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.4/TimeIsUnending.AutoMatchmaking.1.0.4.zip
+tar -xf TimeIsUnending.AutoMatchmaking.1.0.4.zip
+del TimeIsUnending.AutoMatchmaking.1.0.4.zip
 cd ../../..
 echo -multiple -norestrictservercommands +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
 del Northstar.release.v1.11.1.zip
