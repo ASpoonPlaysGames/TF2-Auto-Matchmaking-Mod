d19ea99a291b11a4fbe5e08ca15c8d7c48e4196b
Jack <66967891+ASpoonPlaysGames@users.noreply.github.com>
2023-01-20 03:10:48 +0000

    use a profile to install AMM

--- AutoMatchmakingMod_Installer.bat
+++ AutoMatchmakingMod_Installer.bat
@@ -11,11 +11,11 @@ rmdir /s /q "R2Northstar/plugins/DiscordRPC.dll"
 rmdir /s /q "R2Northstar/mods/Northstar.Client"
 rmdir /s /q "R2Northstar/mods/Northstar.Custom"
 rmdir /s /q "R2Northstar/mods/Northstar.CustomServers"
-mkdir "R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.4"
-cd R2Northstar/mods/TimeIsUnending.AutoMatchmaking.1.0.4
+mkdir "R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.4"
+cd R2Vanilla/mods/TimeIsUnending.AutoMatchmaking.1.0.4
 curl -LO https://github.com/TimeIsUnending/TF2-Auto-Matchmaking-Mod/releases/download/v1.0.4/TimeIsUnending.AutoMatchmaking.1.0.4.zip
 tar -xf TimeIsUnending.AutoMatchmaking.1.0.4.zip
 del TimeIsUnending.AutoMatchmaking.1.0.4.zip
 cd ../../..
-echo -multiple -norestrictservercommands +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
+echo -multiple -norestrictservercommands -profile="R2Vanilla" +ns_has_agreed_to_send_token 2 > ns_startup_args.txt
 del Northstar.release.v1.11.1.zip
